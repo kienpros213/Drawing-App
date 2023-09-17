47c47c88f1db937831e2d25362750d30440ffd0e
kienpros213 <kienpros213@gmail.com>
2023-09-17 14:23:01 +0700

    statble

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Canvas from './components/Canvas'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <Canvas
+        width = {700}
+        height = {500}
+      />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Hooks.js
@@ -0,0 +1,60 @@
+import { useRef } from "react";
+
+export function useOnDraw(onDraw){
+
+    const canvasRef = useRef(null);
+
+    const isDrawingRef = useRef(false);    
+
+    //set canvas reference
+    function setCanvasRef(ref){
+        if(!ref) return;
+        canvasRef.current = ref;
+        initMouseMoveListener();
+        initMouseDownListener();
+        initMouseUpListener();
+    }
+
+    function initMouseMoveListener(){
+        const mouseMoveListener = (e) => {
+            if(isDrawingRef.current){
+                const point = computePointInCanvas(e.clientX, e.clientY);
+                const ctx = canvasRef.current.getContext('2d');
+                if(onDraw) onDraw(ctx, point);
+                console.log(point);
+            }
+        }
+        window.addEventListener("mousemove", mouseMoveListener)
+    }
+
+    function initMouseDownListener(){
+        if(!canvasRef.current) return
+        const listener = () => {
+            isDrawingRef.current = true;
+        }
+        canvasRef.current.addEventListener("mousedown", listener)
+    }
+
+    function initMouseUpListener(){
+        const listener = () => {
+            isDrawingRef.current = false;
+        }
+        canvasRef.current.addEventListener("mouseup", listener)
+    }
+
+    function computePointInCanvas(clientX, clientY){
+        if(canvasRef.current){
+            const boundingRect = canvasRef.current.getBoundingClientRect();
+            return {
+                x: clientX - boundingRect.left,
+                y: clientY - boundingRect.top
+            }
+        }
+        else {
+            return null;
+        }
+    }
+    
+    return setCanvasRef
+
+};
