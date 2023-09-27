ea2db1ffa63ec404e73ec837c39733f7a71a5630
kienpros213 <kienpros213@gmail.com>
2023-09-27 16:49:02 +0700

    added basic login logout

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,43 @@ import { io } from "socket.io-client";
 import "./App.css";
 import Canvas from "./components/Canvas";
 import Room from "./components/Room";
+import Login from "./components/Login";
 
 function App() {
   const [socket, setSocket] = useState(null);
   const [roomName, setRoomName] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
     const initSocket = io("ws://localhost:3000");
     setSocket(initSocket);
   }, []);
 
-  return (
-    <div className="App">
-      <Stack>
-        <Room socket={socket} roomName={roomName} setRoomName={setRoomName} />
-        <Canvas roomName={roomName} socket={socket} width={700} height={500} />
-      </Stack>
-    </div>
-  );
+  if (isLoggedIn) {
+    return (
+      <div className="App">
+        <Stack>
+          <button
+            type="button"
+            onClick={() => {
+              setIsLoggedIn(false);
+            }}
+          >
+            {" "}
+            log out{" "}
+          </button>
+          <Room socket={socket} roomName={roomName} setRoomName={setRoomName} />
+          <Canvas
+            roomName={roomName}
+            socket={socket}
+            width={700}
+            height={500}
+          />
+        </Stack>
+      </div>
+    );
+  }
+  return <Login setIsLoggedIn={setIsLoggedIn} />;
 }
 
 export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,38 +1,23 @@
 import { Stack } from "@chakra-ui/react";
-import axios from "axios";
+import { SendLoginRequest } from "../utils/SendLoginRequest";
 
-function Login() {
-
-    const endpoint = "http://localhost:3030"
-
-    function sendPost() {
-        console.log("" + endpoint + "/auth/login")
-        const username = document.getElementsByClassName("username")
-        const password = document.getElementsByClassName("password")
-        console.log(username[0].value, password[0].value)
-        axios({
-            method: "post",
-            url: "" + endpoint + "/auth/login",
-            data: {
-                "username": username[0].value,
-                "password": password[0].value,
-            }
-        })
-            .then(function (response) {
-                console.log(response.data.access_token);
-            })
-    }
-
-    return (
-        <>
-            <Stack dir="vertical">
-                <input className="username" name="username" placeholder="username" />
-                <input className="password" name="password" placeholder="password" />
-                <button type="button" onClick={sendPost}> submit </button>
-            </Stack>
-        </>
-    );
-
+function Login(props) {
+  return (
+    <>
+      <Stack dir="vertical">
+        <input className="username" name="username" placeholder="username" />
+        <input className="password" name="password" placeholder="password" />
+        <button
+          type="button"
+          onClick={() => {
+            SendLoginRequest(props.setIsLoggedIn);
+          }}
+        >
+          login
+        </button>
+      </Stack>
+    </>
+  );
 }
 
-export default Login
+export default Login;
